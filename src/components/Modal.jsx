c0ea6cb1d739c973e965ea14eaa5cd5e9716ecd8
Máté Bartók <mate.bartok88@gmail.com>
2024-06-02 02:06:24 +0200

    Animating Element Disappearances / Removal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,19 +4,20 @@ import { motion } from "framer-motion";
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
-      <div className="backdrop" onClick={onClose} />
-      <motion.dialog 
-      initial={{
-        opacity: 0,
-        y: -30
-      }}
-      animate={{
-        opacity: 1,
-        bounce: 1,
-        y: 0
-      }}
-      open 
-      className="modal">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="backdrop"
+        onClick={onClose}
+      />
+      <motion.dialog
+        initial={{ opacity: 0, y: -30 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: -30 }}
+        open
+        className="modal"
+      >
         <h2>{title}</h2>
         {children}
       </motion.dialog>
