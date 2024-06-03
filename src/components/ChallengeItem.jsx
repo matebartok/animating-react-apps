901e2d4ea4259131796c0c93154822861d041a79
Máté Bartók <matebartok@Mates-Mac-mini.local>
2024-06-03 17:47:15 +0200

    Combining Animations with layout animations

--- src/components/ChallengeItem.jsx
+++ src/components/ChallengeItem.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 
 import { ChallengesContext } from "../store/challenges-context.jsx";
 
@@ -28,7 +28,7 @@ export default function ChallengeItem({
   }
 
   return (
-    <motion.li layout exit={{y: -30, opacity: 0}}>
+    <motion.li layout exit={{ y: -30, opacity: 0 }}>
       <article className="challenge-item">
         <header>
           <img {...challenge.image} />
@@ -48,21 +48,26 @@ export default function ChallengeItem({
             <button onClick={onViewDetails}>
               View Details{" "}
               <motion.span
-                animate={{ rotate: isExpanded ? 180 : 0}}
+                animate={{ rotate: isExpanded ? 180 : 0 }}
                 className="challenge-item-details-icon"
               >
                 &#9650;
               </motion.span>
             </button>
           </p>
-
-          {isExpanded && (
-            <div>
-              <p className="challenge-item-description">
-                {challenge.description}
-              </p>
-            </div>
-          )}
+          <AnimatePresence>
+            {isExpanded && (
+              <motion.div
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: "auto", opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+              >
+                <motion.p className="challenge-item-description">
+                  {challenge.description}
+                </motion.p>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       </article>
     </motion.li>
